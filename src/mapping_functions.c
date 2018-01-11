ec22bca5beda950b0d71223390556cdb82563b7a
Virgile-Uytterhaegen <uytterhaegen@telecom-paristech.fr>
2018-01-11 14:45:22 +0100

    Now compilates with separated files, run make

---
+++ src/mapping_functions.c
@@ -0,0 +1,114 @@
+//////////////
+// INCLUDES //
+//////////////
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <pthread.h>
+#include <math.h>
+#include "coroutine.h"
+#include "ev3.h"
+#include "ev3_port.h"
+#include "ev3_sensor.h"
+#include "ev3_tacho.h"
+#include "communication.h"
+#include "motor.h"
+
+// WIN32 /////////////////////////////////////////
+#ifdef __WIN32__
+
+#include <windows.h>
+
+// UNIX //////////////////////////////////////////
+#else
+
+#include <unistd.h>
+#define Sleep( msec ) usleep(( msec ) * 1000 )
+
+//////////////////////////////////////////////////
+#endif
+
+
+
+//////////////////////
+// GLOBAL VARIABLES //
+//////////////////////
+
+#define EMPTY 0
+#define OBSTACLE 1
+#define WALL 2
+#define UNDEFINED 3
+
+double x,y;
+
+struct pixel {
+	int x;
+	int y;
+	int R;
+	int G;
+	int B;
+};
+
+pixel map[100][100];
+
+
+//////////////////////
+// MAPPING FUNCTIONS//
+//////////////////////
+
+
+void reset_coord() {
+	x = 60;
+	y = 15;
+}
+
+
+void update_coord(double diff, int angle) {
+	switch (angle) {
+	case 0:
+		x = x+diff;
+		break;
+	case 1:
+		y = y+diff;
+		break;
+	case 2:
+		x = x-diff;
+		break;
+	case 3:
+		y = y-diff;
+		break;
+	}
+}
+
+
+void draw_map(int i, int j, int type) {
+	
+	switch (type) {
+	case EMPTY: 
+		map[i][j]->R=0;
+		map[i][j]->G=0;
+		map[i][j]->B=0;
+
+	case OBSTACLE:
+		map[i][j]->R=0;
+		map[i][j]->G=100;
+		map[i][j]->B=0;
+
+	case WALL:
+		map[i][j]->R=0;
+		map[i][j]->G=0;
+		map[i][j]->B=100;
+
+	case UNDEFINED:
+		map[i][j]->R=100;
+		map[i][j]->G=0;
+		map[i][j]->B=0;
+
+}
+
+
+
+
+
---
+++ src/mapping_functions.h
@@ -0,0 +1,7 @@
+void reset_coord(void);
+
+void update_coord(double diff, int angle);
+
+void draw_map(int i, int j, int type);
+
+
