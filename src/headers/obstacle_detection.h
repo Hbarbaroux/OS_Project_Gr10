1549fdf15d901b4945378e16a01d3a16d67e2da2
Pavollo <pawlakbo@student.agh.edu.pl>
2018-01-15 16:39:04 +0100

    Update obstacle_detection.h

--- src/headers/obstacle_detection.h
+++ src/headers/obstacle_detection.h
@@ -1,7 +1,8 @@
-int there_is_obstacle();
-
-int what_kind_of_obstacle();
-
-int detection();
-
-
+int there_is_obstacle(); //Made by Pawel
+                         //This function returns 1 when the distance value from the ultrasonic sensor is smaller than DETECTION_DISTANCE global variable.
+int what_kind_of_obstacle();//Made by Pawel
+                         //This function returns 1 when the recognized color is red, for movable obstacle.
+int detection();//Made by Pawel
+                         //This function is a combination of previous ones to move the movable obstacle behind the robot.
+int distance_to_obstacle(); //Made by Pawel
+                         // This function is necessary to return the distance to obstacle. It was used for scan functions.
