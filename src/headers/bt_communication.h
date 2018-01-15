6626cb57d5e11d4d88efa85de375698a37148e02
Virgile-Uytterhaegen <uytterhaegen@telecom-paristech.fr>
2018-01-15 16:36:58 +0100

    Added comments

--- src/headers/bt_communication.h
+++ src/headers/bt_communication.h
@@ -1,3 +1,5 @@
+//Functions written by Virgile Uytterhaegen
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -19,12 +21,16 @@
 
 void read_from_server(int sock, char *buffer, size_t maxSize);
 
+//Send the position of the robot to the server
 void send_position(double x, double y);
 
+//Send a MSG_OBSTACLE to the server with the position of the obstacle and if it has been released (act = 0) or picked (act = 1)
 void send_obstacle(double x, double y, int act);
 
+//Send a MSG_MAPDONE to the server
 void send_map_done(void);
 
+//Send the map to the server
 void send_map(int map_type);
 
 int connect_server(void);
