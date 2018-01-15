496e526d0827685a6048787d90c5e4a243355703
Pavollo <pawlakbo@student.agh.edu.pl>
2018-01-15 16:53:24 +0100

    Comments added

--- src/headers/move_obstacle.h
+++ src/headers/move_obstacle.h
@@ -1,7 +1,6 @@
-void _take_front_obstacle(void);
-
-void _release_front_obstacle(void);
-
-void _release_obstacle(void);
-
-
+void _take_front_obstacle(void); // Made by Pawel
+      //This function is used when robot detects the movable obstacle in the front. It put the frame down to grab the obstacle.
+void _release_front_obstacle(void); // Made by Pawel
+      //This function is used to put the front frame into upper position to release the movable obstacle.
+void _release_obstacle(void); // Made by Pawel
+      //This is used to release the nonmovable at the back arm. After releasing the arm is going to initial position. 
