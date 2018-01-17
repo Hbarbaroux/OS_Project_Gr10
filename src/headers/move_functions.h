9fcb49588d9cd786fabfbc1d985a67391067e13e
jz_liang <33424979+liangjz001@users.noreply.github.com>
2018-01-17 13:35:01 +0100

    Update move_functions.h

--- src/headers/move_functions.h
+++ src/headers/move_functions.h
@@ -1,4 +1,6 @@
- void _set_mode( int value );
+//this part is responsible by Jianzhong LIANG
+//used to perceive the current position and make next movement accurately, in charge of the use of motor for wheel and the compass sensor.
+void _set_mode( int value );
 
  void _run_forever( int l_speed, int r_speed );
 
@@ -10,15 +12,23 @@
 
  void _stop( void );
 
-void turn_to_certain_direction (int direc);
+void turn_to_certain_direction (int direc);// by Jianzhong, implement the accurate rotation.
+// accurate rotate with the accuracy of +-1 degree, implement the feedback to keep robot heading to certain degree excatly.
 
-void turn_right_certain_degree(int degree);
+void keep_go_straight (int distance);// by Jianzhong
 
-void turn_left_certain_degree(int degree);
+void turn_right_certain_degree(int degree);// by Jianzhong 
+//for accurately turning, some tuning for general utilization, some trade off in adjust time and speed.
 
-void turn_left(void);
+void turn_left_certain_degree(int degree);// by Jianzhong
+//for accurately turning
 
-void turn_right(void);
+void turn_left(int degree);
+// by Jianzhong, tuning version for scaning version
+
+void turn_right(int degree);
+// by Jianzhong, tuning version for scaning version
+
 
 int searching_obstacle_right_array(Pair *obstacles, int index); //Made by Pawel & modified by Hugo.
 	//Both these functions (this one and the one below) are used to look around for searching obstacles. It turns for and scans by ultrasonic sensor and returns the closest values. After reaching the max angle it goes back to initial position.
